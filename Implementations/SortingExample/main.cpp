a0083e781918b8a5d73f61a7af13950f127f3ec4
Harkirat Singh <65704029+HarkiratSingh7@users.noreply.github.com>
2020-11-24 20:09:28 +0530

    Sorting examples added

---
+++ Implementations/SortingExample/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "sortings.h"
+
+using namespace std;
+using namespace algolib;
+#define DATA_ARR                                                           \
+    {                                                                      \
+        34, 34, 99, -1, 89, 45, 21, 0, 34, 95, 33, 432, 999, 1001, -11, 94 \
+    }
+#define N 16
+inline void Print(int *arr)
+{
+    for (uint32 i = 0; i < N; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+int main()
+{
+    // No need to delete stack allocated arrays
+    int arr1[N] = DATA_ARR;
+    cout << "The array is ";
+    Print(arr1);
+    RUN_CALC_TIME("HeapSort", HeapSort(arr1, N));
+    Print(arr1);
+
+    int arr2[N] = DATA_ARR;
+    RUN_CALC_TIME("BubbleSort", BubbleSort(arr2, N));
+    Print(arr2);
+
+    int arr3[N] = DATA_ARR;
+    RUN_CALC_TIME("QuickSort", BubbleSort(arr3, N));
+    Print(arr3);
+
+    return 0;
+}
