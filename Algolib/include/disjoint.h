75aff3b9e5836e055622252f72cdde71c811fafd
Harkirat Singh <honey.harkirat@outlook.com>
2022-08-03 20:55:18 +0530

    Disjoint Set Implemented

---
+++ Algolib/include/disjoint.h
@@ -0,0 +1,46 @@
+#ifndef DISJOINT_H
+#define DISJOINT_J
+#include "framework.h"
+
+using namespace std;
+
+class DisjointSet
+{
+private:
+    // first -> parent, second -> rank
+    vector<pair<int, int>> subsets;
+    int initialTotal{0};
+
+public:
+    DisjointSet(int individual)
+        : initialTotal(individual)
+    {
+        subsets.assign(individual, {0, 0});
+        for (int i = 0; i < individual; i++)
+            subsets[i].first = i;
+    }
+
+    int find(int x)
+    {
+        if (x != subsets[x].first)
+            subsets[x].first = find(subsets[x].first);
+        return subsets[x].first;
+    }
+
+    void set_union(int x, int y)
+    {
+        int px = find(x);
+        int py = find(y);
+
+        if (subsets[px].second > subsets[py].second)
+            subsets[py].first = px;
+        else
+        {
+            subsets[px].first = py;
+            if (subsets[px].second == subsets[py].second)
+                subsets[py].second++;
+        }
+    }
+};
+
+#endif
